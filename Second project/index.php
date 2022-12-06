11fbbfe7174535a335462b52a9ccf92ed7d75cb1
Yury Belyanin <wildjackal1987@gmail.com>
2022-12-06 21:12:22 +0500

    changes

--- Second project/index.php
+++ Second project/index.php
@@ -16,74 +16,11 @@
 <!-- Отображаемое тело страницы -->
 <body>
 
-<div class="block">
-    <div class="block_row">
-        <div class="block_element block_element_1">
-            <div class="block_content">
-                <h1> Мой первый проект</h1>
-            </div>
-        </div>
-        <div class="block_element block_element_2">
-            <div class="block_content">
-                <p> мое имя
-                    <?php
-                    $name = "Yury";
-                    echo $name;
-                    ?>
-                </p>
-                <p> мой возраст
-                    <?php
-                    $age = "35";
-                    echo $age;
-                    ?>
-                </p>
-                <?php
-                $PI = pi();
-                echo "ПИ=" . $PI;
-                ?>
-                </p>
-                <?php
-                $a = date('H:m:s');/*пробовал функцию времени*/
-                echo $a;
-                ?>
-                <!--первый способ через переменные-->
-                <p>
-                    <?php
-                    $image1 = '<img src="PHP_icons/sun.png" width="300" height="300">';
-                    $image2 = '<img src="PHP_icons/night.jpg"  width="300" height="300">';
+<?php
 
-                    $time = date('H');
-                    if ($time >= 8 && $time < 20) {
-                        echo $image1 /*= 'fact12/PHP_icons/sun.png'*/
-                        ;
-                    } else {
-                        echo $image2/* = 'fact12/PHP_icons/night.jpg'*/
-                        ;
-                    }
-                    ?>
-                </p>
-                <!--Второй споссоб через просто вывод-->
-                <p>
-                    <?php
-                    $time = date('H');
 
-                    if ($time >= 8 && $time < 20) {
-                        echo '<img src="PHP_icons/sun.png" width="300" height="300">';
-                    } else {
-                        echo '<img src="PHP_icons/night.jpg"  width="300" height="300">';
-                    }
-                    ?>
-                </p>
 
-            </div>
-        </div>
-        <div class="block_element block_element_3">
-            <div class="block_content">
-                <p>COPYRIGHT BY MY</p>
-            </div>
-        </div>
-    </div>
-</div>
+?>
 
 
 </body>
